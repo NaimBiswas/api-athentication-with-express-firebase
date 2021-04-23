de2edeb2b455b6122657a3efdccd4cc5be740498
NaimBiswas <nayeembiswas2@gmail.com>
2021-04-23 12:05:57 +0600

    fetch job from database done

--- jobs/jobs.js
+++ jobs/jobs.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const Router = express.Router();
+const { dataBase } = require("../src/firebase")
 // get All Jobs 
 Router.get('/', async (req, res) => {
    try {
-      res.send("This is job routes");
+      dataBase.ref("jobs").once("value")
+         .then((data) => {
+            if (data.val()) {
+               console.log(data.val());
+               res.send(data.val())
+            } else {
+               res.send("NO JOBS FOUNDS")
+               console.log("NO JOBS FOUNDS")
+            }
+         }).catch(err => {
+            console.log(err);
+         })
    } catch (error) {
-
+      console.log(error);
    }
 })
 
